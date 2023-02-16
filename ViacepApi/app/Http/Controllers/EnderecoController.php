d2e74c396721435560595ea6dca22ba9c81e57c6
ItaloVeras11 <italocabralveras@gmail.com>
2023-02-16 11:48:22 -0300

    First Commit

---
+++ ViacepApi/app/Http/Controllers/EnderecoController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class EnderecoController extends Controller
+{
+    public function index()
+    {
+        return view('busca');
+    }
+
+    public function buscar(Request $request)
+    {
+        $cep = $request->input('cep');
+        $response = Http::get("viacep.com.br/ws/$cep/json/")->json();
+
+        return view('adicionar')->with([
+            'cep' => $request->input('cep'),
+            'logradouro' => $response['logradouro'],
+            'bairro' =>$response['bairro'],
+            'cidade' =>$response['localidade'],
+            'estado' =>$response['uf'],
+        ]);
+        
+    }
+}
